a2f62fcffb7fd9d40b7f432560a390b5a6e9391b
ebitariere <tarierewodu@gmail.com>
2026-04-05 11:55:58 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# learnable-task-4
+# Title: Customer Burger Ordering System
+  
+## Problem Domain Description
+This repository contains the implementation of amn ordering system for the customers of a burger restaurant 
+
+## Features
+
+### Classes and their functionalities
+
+### 1. **BurgerDelivery**
+- Attributes :
+ - 
---
+++ burgerOrderingSystem.js
@@ -0,0 +1,70 @@
+class burgerDelivery{
+    static totalorders = 0
+    static availableBurgers= 10
+    constructor(meal, price){
+        this.meal= meal;
+        this.price= price;
+        burgerDelivery.totalorders++
+    
+    }
+
+    //static//
+    static orderSlots (){
+        if (burgerDelivery.availableBurgers > 0 ){
+            burgerDelivery.availableBurgers--;
+            console.log(`Order taken! There are now ${burgerDelivery.availableBurgers} slots remaining!`)
+        } else {
+            console.log("Sorry, there are no more burgers available.")
+        }
+    }
+}
+// CUSTOMERS//
+class customers{
+    static totalCustomers = 0;
+
+    constructor(customerEmail, name){
+        this.customerEmail = customerEmail;
+        this.name = name;
+        customers.totalCustomers++;
+    }
+
+    order(){
+        console.log(`Hello,${this.name} your order is being processed...`)
+    }
+
+    orderComplete(){
+        console.log(`Hi, ${this.name}, your order has been processed and your email: ${this.customerEmail} has been recorded`);
+    }
+}
+
+// USAGE
+
+const firstOrder = new burgerDelivery("Agege Burger", 60.99);
+const firstCustomer= new customers("JaneDoe@gmail.com","Jane");
+
+const secondOrder = new burgerDelivery("Cheese Burger", 40.00);
+const secondCustomer= new customers("JohnDoe@gmail.com", "John");
+
+const thirdOrder = new burgerDelivery("Chicken Burger", 50.99);
+const thirdCustomer= new customers("Jessica@gmail.com","Jessica");
+
+firstCustomer.order();
+firstCustomer.orderComplete();
+
+console.log('------------------------')
+console.log('------------------------')
+
+secondCustomer.order();
+secondCustomer.orderComplete();
+
+console.log('------------------------')
+console.log('------------------------')
+
+thirdCustomer.order();
+thirdCustomer.orderComplete();
+
+console.log('------------------------')
+console.log('------------------------')
+
+console.log(`Total burgers ordered: ${burgerDelivery.totalorders}`);
+console.log(`Total customers registered: ${customers.totalCustomers}`);
